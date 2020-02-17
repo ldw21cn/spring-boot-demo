3093998a7220da7417b1980a03ee0683f448b730
lvdawei <daweilv@pateo.com.cn>
2020-02-17 12:27:14 +0800

    feature: 增加 spring-boot-demo-validator 模块

---
+++ spring-boot-demo-validator/src/test/java/tech/liangfang/springbootdemovalidator/SpringBootDemoValidatorApplicationTests.java
@@ -0,0 +1,14 @@
+package tech.liangfang.springbootdemovalidator;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoValidatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
