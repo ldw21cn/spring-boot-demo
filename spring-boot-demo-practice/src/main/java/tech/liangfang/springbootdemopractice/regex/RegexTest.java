0694c35b283877fdb1b15e3d208f6d9f36622d66
lvdawei <daweilv@pateo.com.cn>
2020-04-26 10:28:25 +0800

    fix: 增加正则匹配

---
+++ spring-boot-demo-practice/src/main/java/tech/liangfang/springbootdemopractice/regex/RegexTest.java
@@ -0,0 +1,27 @@
+package tech.liangfang.springbootdemopractice.regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 功能描述：
+ *
+ * @Author dawei.lv  -  daweilv@pateo.com.cn
+ * @Date 2020/4/26 10:21
+ */
+public class RegexTest {
+    public static void main(String[] args) {
+
+        String reg = "(.*)(好的|可以|可以的|行|行吧)(.*)";
+
+        Pattern compile = Pattern.compile(reg);
+        Matcher matcher = compile.matcher("好的我一会过去");
+
+        if (matcher.find() && matcher.groupCount() > 0) {
+            String s = matcher.group(3);
+            System.out.println(s);
+        }
+
+
+    }
+}
